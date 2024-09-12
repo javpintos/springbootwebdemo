1f5487af1ca00fb6eb8c16666466ed0fcb991e4c
javpintos <jpintos234@gmail.com>
2024-09-12 12:16:54 -0300

    index view with ModelAndView ready

--- src/main/java/com/cf/ejemplos/controllers/IndexController.java
+++ src/main/java/com/cf/ejemplos/controllers/IndexController.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
-
 
 @Controller
 public class IndexController {
@@ -18,9 +19,25 @@ public class IndexController {
 		model.addAttribute("titulo", "Soy el titulo");
 		model.addAttribute("saludo", "Hola mundo!!!");
 		model.addAttribute("show", false);
-		List<String> series = List.of("Game of thrones", "Vikings", "Dexter");//null;
-		model.addAttribute("series", series);
+		//List<String> series = List.of("Game of thrones", "Vikings", "Dexter");//null;
+		//model.addAttribute("series", series);
 		return "index";
+	}
+	
+	@GetMapping(value="/index-mv")
+	public ModelAndView indexMV(ModelAndView mv) {
+		mv.addObject("titulo", "Titulo con MV");
+		mv.addObject("saludo", "Saludo con MV");
+		mv.addObject("show", true);
+		//List<String> series = List.of("Game of thrones", "Vikings", "Dexter");//null;
+		//mv.addObject("series", series);
+		mv.setViewName("index");
+		return mv;
+	}
+	
+	@ModelAttribute("series")
+	public List<String> getSeries(){
+		return List.of("Game of thrones", "Vikings", "Dexter", "The last of us");
 	}
 	
 	/*
